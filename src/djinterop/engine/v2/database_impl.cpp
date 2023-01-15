8b762a8aad69160975ad6ddb892152e3bb33dcc5
Adam Szmigin <smidge@xsco.net>
2023-01-15 14:01:06 +0000

    Update runners for both Ubuntu 20.04/22.04

--- src/djinterop/engine/v2/database_impl.cpp
+++ src/djinterop/engine/v2/database_impl.cpp
@@ -34,31 +34,37 @@ database_impl::database_impl(std::shared_ptr<engine_library> library) :
 
 transaction_guard database_impl::begin_transaction()
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
 stdx::optional<crate> database_impl::crate_by_id(int64_t id)
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
 std::vector<crate> database_impl::crates()
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
 std::vector<crate> database_impl::crates_by_name(const std::string& name)
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
 crate database_impl::create_root_crate(std::string name)
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
 track database_impl::create_track(const track_snapshot& snapshot)
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
@@ -74,42 +80,50 @@ void database_impl::verify()
 
 void database_impl::remove_crate(crate cr)
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
 void database_impl::remove_track(track tr)
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
 std::vector<crate> database_impl::root_crates()
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
 stdx::optional<crate> database_impl::root_crate_by_name(const std::string& name)
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
 stdx::optional<track> database_impl::track_by_id(int64_t id)
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
 std::vector<track> database_impl::tracks()
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
 std::vector<track> database_impl::tracks_by_relative_path(
     const std::string& relative_path)
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
 std::string database_impl::uuid()
 {
+    // TODO
     throw std::runtime_error{"Not implemented yet"};
 }
 
