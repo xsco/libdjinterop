1d881afceca1337de69adb183d07076a17494e0b
Adam Szmigin <smidge@xsco.net>
2023-09-16 00:02:26 +0100

    Schema support up to firmware/DJ 3.2.0

--- include/djinterop/engine/engine.hpp
+++ include/djinterop/engine/engine.hpp
@@ -127,22 +127,32 @@ constexpr const engine_version os_2_4_0{
     semantic_version{2, 4, 0}, engine_database_type::os, "Engine DJ OS 2.4.0",
     semantic_version{2, 20, 2}};
 
-/// Engine DJ Desktop 3.0.0
+/// Engine DJ Desktop 3.0.0, 3.0.1
 constexpr const engine_version desktop_3_0_0{
     semantic_version{3, 0, 0}, engine_database_type::desktop,
-    "Engine DJ Desktop 3.0.0", semantic_version{2, 20, 3}};
+    "Engine DJ Desktop 3.0.0 to 3.0.1", semantic_version{2, 20, 3}};
 
-/// Engine DJ OS 3.0.0
+/// Engine DJ OS 3.0.0, 3.0.1
 constexpr const engine_version os_3_0_0{
-    semantic_version{3, 0, 0}, engine_database_type::os, "Engine DJ OS 3.0.0",
+    semantic_version{3, 0, 0}, engine_database_type::os, "Engine DJ OS 3.0.0 to 3.0.1",
     semantic_version{2, 20, 3}};
 
+/// Engine DJ desktop 3.1.0 to 3.2.0
+constexpr const engine_version desktop_3_1_0{
+    semantic_version{3, 1, 0}, engine_database_type::desktop,
+    "Engine DJ Desktop 3.1.0 to 3.2.0", semantic_version{2, 21, 0}};
+
+/// Engine DJ OS 3.1.0 to 3.2.0
+constexpr const engine_version os_3_1_0{
+    semantic_version{3, 1, 0}, engine_database_type::desktop,
+    "Engine DJ OS 3.1.0 to 3.2.0", semantic_version{2, 21, 0}};
+
 /// Set of available versions.
-constexpr const std::array<engine_version, 19> all_versions{
+constexpr const std::array<engine_version, 21> all_versions{
     os_1_0_0,      os_1_0_3,      desktop_1_1_1, os_1_2_0,      os_1_2_2,
     desktop_1_2_2, os_1_3_1,      os_1_4_0,      os_1_5_1,      desktop_1_5_1,
     os_1_6_0,      desktop_2_0_0, os_2_0_0,      desktop_2_2_0, os_2_2_0,
-    desktop_2_4_0, os_2_4_0,      desktop_3_0_0, os_3_0_0};
+    desktop_2_4_0, os_2_4_0,      desktop_3_0_0, os_3_0_0, desktop_3_1_0, os_3_1_0};
 
 /// Set of available V1 versions.
 constexpr const std::array<engine_version, 11> all_v1_versions{
@@ -150,18 +160,18 @@ constexpr const std::array<engine_version, 11> all_v1_versions{
     os_1_3_1, os_1_4_0, os_1_5_1,      desktop_1_5_1, os_1_6_0};
 
 /// Set of available V2 versions.
-constexpr const std::array<engine_version, 8> all_v2_versions{
-    desktop_2_0_0, os_2_0_0, desktop_2_2_0, os_2_2_0,
-    desktop_2_4_0, os_2_4_0, desktop_3_0_0, os_3_0_0};
+constexpr const std::array<engine_version, 10> all_v2_versions{
+    desktop_2_0_0, os_2_0_0,      desktop_2_2_0, os_2_2_0,     desktop_2_4_0,
+    os_2_4_0,      desktop_3_0_0, os_3_0_0,      desktop_3_1_0, os_3_1_0};
 
 /// The most recent version supported by the library.
-constexpr engine_version latest = os_3_0_0;
+constexpr engine_version latest = os_3_1_0;
 
 /// The most recent V2 version supported by the library.
-constexpr engine_version latest_v2 = os_3_0_0;
+constexpr engine_version latest_v2 = os_3_1_0;
 
 /// The most recent OS-type version supported by the library.
-constexpr engine_version latest_os = os_3_0_0;
+constexpr engine_version latest_os = os_3_1_0;
 
 namespace standard_pad_colors
 {
